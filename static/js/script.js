ec6b1326d22d5cec31957a8c01b4c3dfb60717da
bryankazuro04 <mhdalana211201@gmail.com>
2024-08-22 12:18:07 +0700

    create event when chatbot button open & close

--- static/js/script.js
+++ static/js/script.js
@@ -4,9 +4,12 @@ import { handleChat } from "./utils/handleChat.js";
 /**
  *  Queries Elements
  */
+const chatbotContainer = document.querySelector(".chatbot");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
+const closeBtn = document.querySelector(".close-btn");
+const openChatBubbleBtn = document.querySelector(".chatbot-bubble-btn");
 
 /**
  *  API Config
@@ -16,23 +19,46 @@ const API_KEY = "API-KEY";
 const API_URL_OPENAI = "https://api.openai.com/v1/chat/completions";
 const API_URL_DATASET = "/predict";
 
-if (location.pathname === "/chat-bot") {
-  const inputInitHeight = chatInput.scrollHeight; // Get input height value
+const inputInitHeight = chatInput.scrollHeight; // Get input height value
 
-  chatInput.addEventListener("input", () => {
-    chatInput.style.height = `${inputInitHeight}px`;
-    chatInput.style.height = `${chatInput.scrollHeight}px`;
-  });
+chatInput.addEventListener("input", () => {
+  chatInput.style.height = `${inputInitHeight}px`;
+});
 
-  chatInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-      e.preventDefault();
-      handleChat(API_URL_OPENAI, API_KEY, API_URL_DATASET, chatInput, chatbox, inputInitHeight, userMessage);
-    }
-  });
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    e.preventDefault();
+    handleChat(API_URL_OPENAI, API_KEY, API_URL_DATASET, chatInput, chatbox, inputInitHeight, userMessage);
+  }
+});
 
-  sendChatBtn.addEventListener("click", handleChat(API_URL_OPENAI, API_KEY, API_URL_DATASET, chatInput, chatbox, inputInitHeight, userMessage));
-}
+sendChatBtn.addEventListener("click", handleChat(API_URL_OPENAI, API_KEY, API_URL_DATASET, chatInput, chatbox, inputInitHeight, userMessage));
+
+closeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  chatbotContainer.classList.add("animating-close");
+
+  setTimeout(() => {
+    chatbotContainer.classList.remove("animating-close");
+    chatbotContainer.classList.add("hidden");
+  }, 700);
+
+  setTimeout(() => {
+    openChatBubbleBtn.classList.remove("hidden");
+  }, 500);
+});
+
+openChatBubbleBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  chatbotContainer.classList.remove("hidden");
+  chatbotContainer.classList.add("animating-open");
+
+  openChatBubbleBtn.classList.add("hidden");
+
+  setTimeout(() => {
+    chatbotContainer.classList.remove("animating-open");
+  }, 700);
+});
 
 // document.addEventListener("DOMContentLoaded", () => {
 //   // typeWriterEffect();
