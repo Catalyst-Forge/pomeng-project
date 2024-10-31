5eb6af59bc63a6577a210d81e4629cf12841b06b
bryankazuro04 <mhdalana211201@gmail.com>
2024-10-31 16:46:53 +0700

    add username & fullname field and validate username function

--- forms.py
+++ forms.py
@@ -2,12 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo
 
+def validate_username(form, field):
+    if ' ' in field.data:
+        raise ValidationError("Username tidak boleh mengandung spasi.")
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired(), validate_username])
+    fullname = StringField('fullname', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
