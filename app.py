a925c4517882d525f149e12cb156ac74aede0cc3
BagasAuliaAlfasyam <bagas.190180087@mhs.unimal.ac.id>
2024-11-05 14:33:22 +0700

    rename word

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from route import route_bp  # Import blueprint
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SECRET_KEY'] = 'Belekali'
 
     # Initialize extensions
     db.init_app(app)
