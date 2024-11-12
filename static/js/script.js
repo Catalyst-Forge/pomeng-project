c4f6e40fa2eb92d274faee884a73aa0a38462209
bryankazuro04 <mhdalana211201@gmail.com>
2024-11-12 19:59:10 +0700

    remove generateBreadcrumb function

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,8 @@
 import { handleChat } from "./utils/handleChat.js";
 import { CRUDDatasetHandler } from "./utils/crudDatasetHandler.js";
+import { generateBreadcrumb } from "./utils/generateBreadcrumb.js";
 
 const currentPage = window.location.pathname;
-
-// Breadcrumb Link Generate Function
-const generateBreadcrumb = () => {
-  const breadcrumbContainer = document.getElementById("breadcrumb");
-  breadcrumbContainer.innerHTML = ""; // Clear existing items
-
-  // Get path segments and start building HTML
-  const pathSegments = currentPage.split("/").filter(Boolean);
-  let accumulatedPath = "";
-
-  // Loop through segments and add to breadcrumb
-  pathSegments.forEach((segment, index) => {
-    accumulatedPath += `/${segment}`;
-    const isLastSegment = index === pathSegments.length - 1;
-    const segmentName = segment.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
-
-    // Set last segment as active
-    breadcrumbContainer.innerHTML += isLastSegment
-      ? `<li class="breadcrumb-item active fw-bold" aria-current="page">${segmentName}</li>`
-      : `<li class="breadcrumb-item"><a href="${accumulatedPath}" class="link-info text-decoration-none breadcrumb-link">${segmentName}</a></li>`;
-  });
-};
 
 /**
  *  Function for Home Page
@@ -170,11 +149,9 @@ if (currentPage.startsWith("/dashboard")) {
     }
 
     // Generate breadcrumb on page load
-    generateBreadcrumb();
+    generateBreadcrumb(currentPage);
 
     breadcrumbLinks.forEach((bcLink) => {
-      console.log(bcLink.href.endsWith("/dashboard/dataset/fine-tuning"));
-
       if (bcLink.href.endsWith("/dashboard/dataset/fine-tuning")) {
         bcLink.setAttribute("href", "/dashboard/dataset/fine-tuning/list");
       }
