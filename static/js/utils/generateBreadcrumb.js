fd13c45afce0fc32c781d41770b74758a5730342
bryankazuro04 <mhdalana211201@gmail.com>
2024-11-28 15:29:12 +0700

    remodif for breadcrumb link is id

--- static/js/utils/generateBreadcrumb.js
+++ static/js/utils/generateBreadcrumb.js
@@ -11,11 +11,20 @@ export const generateBreadcrumb = (currentPage) => {
   pathSegments.forEach((segment, index) => {
     accumulatedPath += `/${segment}`;
     const isLastSegment = index === pathSegments.length - 1;
+
+    // Format segment name (capitalize and replace dashes with spaces)
     const segmentName = segment.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
 
-    // Set last segment as active
-    breadcrumbContainer.innerHTML += isLastSegment
-      ? `<li class="breadcrumb-item active fw-bold" aria-current="page">${segmentName}</li>`
-      : `<li class="breadcrumb-item"><a href="${accumulatedPath}" class="link-info text-decoration-none breadcrumb-link">${segmentName}</a></li>`;
+    // Check if segment is numeric (assume it's an ID)
+    const isIdSegment = /^\d+$/.test(segment);
+
+    // Set last segment as active or disable numeric segments
+    if (isLastSegment) {
+      breadcrumbContainer.innerHTML += `<li class="breadcrumb-item active fw-bold" aria-current="page">${segmentName}</li>`;
+    } else if (isIdSegment) {
+      breadcrumbContainer.innerHTML += `<li class="breadcrumb-item"><span class="text-body-secondary breadcrumb-link">${segmentName}</span></li>`;
+    } else {
+      breadcrumbContainer.innerHTML += `<li class="breadcrumb-item"><a href="${accumulatedPath}" class="link-info text-decoration-none breadcrumb-link">${segmentName}</a></li>`;
+    }
   });
 };
