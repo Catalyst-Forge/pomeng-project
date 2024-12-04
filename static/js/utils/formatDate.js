c5a28dd5cc92a61728375295d3dd2104ef25add8
bryankazuro04 <mhdalana211201@gmail.com>
2024-12-04 11:48:32 +0700

    delete unused file

--- static/js/utils/formatDate.js
+++
@@ -1,4 +0,0 @@
-// Utility function to format date
-export const formatDate = (dateString) => {
-  return new Date(dateString).toLocaleString();
-};
--- static/js/utils/setFormEnable.js
+++
@@ -1,8 +0,0 @@
-export const setFormEnabled = (enabled) => {
-  const form = document.getElementById("model-form");
-  const trainButton = document.getElementById("train-button");
-  const elements = form.elements;
-
-  for (let element of elements) element.disabled = !enabled;
-  trainButton.disabled = !enabled;
-};
